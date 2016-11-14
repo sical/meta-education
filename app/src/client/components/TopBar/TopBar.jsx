66fa3d1a95a885a7112f31ef5923479476766c9f
Clément Renaud <clement.renaud@gmail.com>
2016-11-14 10:37:01 +0100

    move TopBar to subfolder

---
+++ app/src/client/components/TopBar/TopBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import IconButton from 'material-ui/IconButton'
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+// import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar'
+
+import UserMenu from './UserMenu.jsx'
+
+
+const styles = {
+  topBar : {
+    backgroundColor : 'rgba(0,0,0,.2)'
+  },
+  buttonToggleSideNav : {
+    marginRight: '200px'
+  }
+}
+
+export default class TopBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    let iconToggleSideNav = <IconButton
+      onClick={this.props.onHomeButtonClick}
+      >
+      <NavigationMenu/>
+    </IconButton>
+
+    return (
+      <AppBar
+        title="MetaEducation"
+        iconElementLeft={iconToggleSideNav}
+        />
+    )
+  }
+}
