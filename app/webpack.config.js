b305019bc1052ef646645776f45a424d8e599c8b
Clément Renaud <clement.renaud@gmail.com>
2016-11-28 14:06:58 +0100

    optimize webpack production bundle size

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -8,17 +8,16 @@ var envPlugin = new webpack.DefinePlugin({
 })
 
 if(process.env.NODE_ENV === 'production'){
+  console.log('production mode');
   var plugins = [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js','common.js'),
+    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js','common.js'),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: true },
-      comments: false,
-      sourceMap: false,
-      mangle: true,
-      minimize: true
+      compress: { warnings: false }
     }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 15}),
+    new webpack.optimize.MinChunkSizePlugin({minChunkSize: 10000}),
     new webpack.optimize.AggressiveMergingPlugin(),
     // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
@@ -48,24 +47,22 @@ module.exports = {
     loaders: [ {
         test: /\.js$/,
         loaders: ['babel'],
-        plugins: plugins,
         exclude: [node_modules_dir]
       },{
         test: /\.jsx?$/,
         include: path.join(__dirname, 'src'),
         loader: 'babel-loader',
-        plugins: plugins,
         query: {
           presets: ['es2015', 'react', 'stage-0']
         },
         exclude: [node_modules_dir]
       },{
         test: /\.css$/,
-        loader: 'style!css?modules',
-        include: /flexboxgrid/,
+        loader: 'style!css?modules'
       }
     ]
   },
+  plugins: plugins,
   resolveLoader: {
     root: path.join(__dirname, 'node_modules')
   }
